926557387177b9abffd9adbca294e7d2ebc5b076
Danislav <danislavkostolov@gmail.com>
2022-04-16 18:04:57 +0300

    JS DOM

---
+++ JavaScript DOM/robot.js
@@ -0,0 +1,73 @@
+const form = document.querySelector("form");
+const robotPhrase = form.querySelector("textarea#phrase");
+const robotType = form.querySelector("select#select-type");
+const robotName = form.querySelector("input#name");
+const color = form.querySelector("input#color-picker");
+const canTalk = form.querySelector("input#talk");
+const canJump = form.querySelector("input#jump");
+const canBlink = form.querySelector("input#blink");
+
+robotPhrase.disabled = true;
+robotType.value = "";
+
+const robotList = [];
+
+form.addEventListener("submit", createRobot);
+
+function onCheckboxChange() {
+  const checkbox = form.querySelector("input[name='talk']");
+  robotPhrase.disabled = !checkbox.checked;
+}
+
+function createRobot(event) {
+  if (robotName.value && robotType.value && color.value && (!canTalk.checked || canTalk.checked && robotPhrase.value)) { 
+    const robot = {Name: robotName.value, Type: robotType.value, Color: color.value, Phrase: robotPhrase.value, jump: canJump.checked, talk: canTalk.checked, blink: canBlink.checked};
+    robotList.push(robot);
+
+    createRobotSection();
+
+    robotName.value = "";
+    robotType.value = "";
+    robotPhrase.value = "";
+    robotPhrase.disabled = true;
+    canJump.checked = false;
+    canTalk.checked = false;
+    canBlink.checked = false;
+    color.value = "#e96126";
+  } else {
+    alert("Form is not full.");
+  }
+  event.preventDefault();
+}
+
+function createRobotSection(){
+  const sections = document.querySelectorAll(".factory-section");
+  const element = document.querySelector("#slide-1");
+  const clone = element.cloneNode(true);
+  clone.id = `slide-${sections.length + 1}`;
+
+  const newRobot = clone.querySelector(".robot-container");
+  newRobot.classList.toggle(robotType.value === "Male" ? "male" : "female");
+  if (canJump.checked == true) {
+    newRobot.classList.toggle("can-jump");
+  }
+  if (canTalk.checked == true) {
+    newRobot.classList.toggle("can-talk");
+  }
+  if (canBlink.checked == true) {
+    newRobot.classList.toggle("can-blink");
+  }
+  const newRobotName = newRobot.querySelector(".robot-name").innerHTML = robotName.value;
+  const newRobotColor = newRobot.querySelector(".body").style.backgroundColor = color.value;
+  const newText = clone.querySelector("h2").innerHTML = `${robotType.value} Robot`;
+  const newRobotPhrase = newRobot.querySelector(".bubble").innerHTML = robotPhrase.value;
+
+  clone.classList.add("slide");
+
+  element.after(clone);
+
+  const buttons = document.querySelector(".buttons");
+  buttons.style.display = "inline-block";
+
+  showSlide(currentSlide);
+}
---
+++ JavaScript DOM/slider.js
@@ -0,0 +1,30 @@
+let currentSlide = 1;
+const prev = document.querySelector(".prev")
+const next = document.querySelector(".next")
+
+function showSlide(slideIndex) {
+  const slides = document.getElementsByClassName('slide');
+  if (slideIndex > slides.length) { currentSlide = 1 }
+  if (slideIndex < 1) { currentSlide = slides.length }
+  for (var i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none'
+  }
+  slides[currentSlide - 1].style.display = 'block';
+
+  if(slides.length <= 1) {
+    prev.disabled = true;
+    next.disabled = true;
+  }
+  if (slides.length > 1) {
+    prev.disabled = false;
+    next.disabled = false;
+  }
+}
+
+function nextSlide() {
+  showSlide(currentSlide += 1);
+}
+
+function previousSlide() {
+  showSlide(currentSlide -= 1);
+}
